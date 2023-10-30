7b5abaa82cfa4b79c54f57825d915a1477366894
Rey <r.rihan60@yahoo.com>
2023-10-30 11:18:13 -0700

    added message for smaller screens

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ export const StudentNumber = createContext(1);
 
 function App() {
   const [studentNumber, setStudentNumber] = useState(0);
+  const [smallScreen, setSmallScreen] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+  }, [])
+
+  const handleResize = () => {
+    console.log(window.innerWidth)
+    if(window.innerWidth < 900){
+      setSmallScreen(true);
+    }
+    else{
+      setSmallScreen(false);
+    }
+  }
 
 
   return (
     <StudentNumber.Provider value={studentNumber}>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login setStudentNumber={setStudentNumber} />}  />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={smallScreen ? <h5 className='small-message'>Please visit the dashboard on a larger screen or make your browser screen wider. </h5> : <Login setStudentNumber={setStudentNumber} />}  />
+      <Route path="/dashboard" element={smallScreen ? <h5 className='small-message'>Please visit the dashboard on a larger screen or make your browser screen wider. </h5> : <Dashboard />} />
     </Routes>
     </BrowserRouter>
     </StudentNumber.Provider>
