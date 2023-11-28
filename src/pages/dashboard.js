2e365eb1457d8e520c173d56383985fc562c8885
Rey <r.rihan60@yahoo.com>
2023-11-28 10:46:45 -0800

    removed 301543692

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -18,9 +18,9 @@ import baseURL from '../variables';
 function Dashboard(props){
     const blacklist = [
         "301604458",
-        // "301541572",
+        "301541572",
         "301563520",
-        "301543692"
+        // "301543692"
     ];
     const studentNumber = useContext(StudentNumber)
     const { state } = useLocation();
