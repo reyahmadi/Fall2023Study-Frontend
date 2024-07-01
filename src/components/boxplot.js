a7be7d15af20e8a9617c7b8e5dbd320435dd9ef3
Rey <r.rihan60@yahoo.com>
2024-07-01 15:35:11 -0700

    Fixed boxplot

--- src/components/boxplot.js
+++ src/components/boxplot.js
@@ -5,6 +5,32 @@ import HighchartsReact from 'highcharts-react-official'
 import HighchartsMore from 'highcharts/highcharts-more';
 HighchartsMore(Highcharts);
 
+function getPercentile(data, percentile) {
+  data.sort(numSort);
+  var index = (percentile/100) * data.length;
+  var result;
+  if (Math.floor(index) == index) {
+       result = (data[(index-1)] + data[index])/2;
+  }
+  else {
+      result = data[Math.floor(index)];
+  }
+  return result;
+}
+//because .sort() doesn't sort numbers correctly
+function numSort(a,b) { 
+  return a - b; 
+} 
+
+function getBoxValues(data) {
+  var boxValues = {};
+  boxValues.low    = Math.min.apply(Math,data);
+  boxValues.q1     = getPercentile(data, 25);
+  boxValues.median = data.reduce((acc,v,i,a)=>(acc+v/a.length),0).toFixed(2)
+  boxValues.q3     = getPercentile(data, 75);
+  boxValues.high   = Math.max.apply(Math,data);
+  return boxValues;
+}
 
 
 const BoxPlot = (props) => {
@@ -38,7 +64,7 @@ const BoxPlot = (props) => {
         type: 'boxplot',
         pointWidth: 20,
         data: [
-          props.data
+          getBoxValues(props.data)
         ],
         tooltip: {
             headerFormat: '<em>Experiment No {point.key}</em><br/>'
